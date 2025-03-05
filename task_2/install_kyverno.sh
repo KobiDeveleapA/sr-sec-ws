1bef4cd15d27b9bbe80275386428eeb3fa701391
Kobi Avshalom <kobiavshalom@Kobis-MacBook-Pro.local>
2025-03-06 00:27:48 +0200

    kyverno installation script

---
+++ task_2/install_kyverno.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
+helm repo add kyverno https://kyverno.github.io/kyverno/
+helm repo update
+helm install kyverno kyverno/kyverno
+
+echo -e "${YELLOW}=== Waiting for Kyverno pods to be ready ===${NC}"
+kubectl wait --for=condition=ready pod -l app.kubernetes.io/name=kyverno -n kyverno --timeout=120s
